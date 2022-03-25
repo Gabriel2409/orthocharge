afc9321332eab7ebbc419176ee977ae5be3ff9a4
Gabriel2409 <gabriel.cournelle@gmail.com>
2022-03-25 12:55:19 +0100

    added homepage component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,8 +4,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { HomePageComponent } from './home-page/home-page.component';
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomePageComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
