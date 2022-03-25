77fe9b0a680f49fc0cd9750a0168710c2de25997
Gabriel2409 <gabriel.cournelle@gmail.com>
2022-03-25 15:10:51 +0100

    email password login

---
+++ frontend/src/app/user/email-login/email-login.component.ts
@@ -0,0 +1,104 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+
+@Component({
+  selector: 'app-email-login',
+  templateUrl: './email-login.component.html',
+  styleUrls: ['./email-login.component.scss'],
+})
+export class EmailLoginComponent implements OnInit {
+  form: FormGroup | null = null;
+  formType: FormType = 'login';
+  loading: boolean = false;
+  serverMessage: string | null = null;
+  showPassword: boolean = false;
+  showConfirmPassword: boolean = false;
+
+  constructor(
+    private angularFireAuth: AngularFireAuth,
+    private formBuilder: FormBuilder
+  ) {}
+
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.minLength(6), Validators.required]],
+      passwordConfirm: ['', []],
+    });
+  }
+
+  changeType(val: FormType) {
+    this.formType = val;
+  }
+
+  get isLogin(): boolean {
+    return this.formType === 'login';
+  }
+  get isSignup(): boolean {
+    return this.formType === 'signup';
+  }
+  get isPasswordReset(): boolean {
+    return this.formType === 'reset';
+  }
+
+  get email(): AbstractControl | null | undefined {
+    return this.form?.get('email');
+  }
+  get password(): AbstractControl | null | undefined {
+    return this.form?.get('password');
+  }
+  get passwordConfirm(): AbstractControl | null | undefined {
+    return this.form?.get('passwordConfirm');
+  }
+
+  get passwordDoesMatch(): boolean {
+    if (this.formType !== 'signup') return true;
+    if (!this.password || !this.passwordConfirm) return false;
+
+    return this.password.value === this.passwordConfirm.value;
+  }
+
+  async onSubmit() {
+    if (!this.email || !this.password) return;
+    this.loading = true;
+    const email = this.email.value;
+    const password = this.password.value;
+    try {
+      if (this.isLogin) {
+        await this.angularFireAuth.signInWithEmailAndPassword(email, password);
+        this.loading = false;
+      }
+      if (this.isSignup) {
+        await this.angularFireAuth.createUserWithEmailAndPassword(
+          email,
+          password
+        );
+        this.loading = false;
+      }
+
+      if (this.isPasswordReset) {
+        await this.angularFireAuth.sendPasswordResetEmail(email);
+        this.serverMessage = 'Check your email';
+        this.loading = false;
+      }
+    } catch (err: any) {
+      this.serverMessage = err;
+      this.loading = false;
+    }
+  }
+
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
+  }
+  toggleConfirmPasswordVisibility() {
+    this.showConfirmPassword = !this.showConfirmPassword;
+  }
+}
+
+type FormType = 'login' | 'signup' | 'reset';
--- frontend/src/app/user/user.module.ts
+++ frontend/src/app/user/user.module.ts
@@ -1,15 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from '../shared/shared.module';
-import { UserRoutingModule } from './user-routing.module';
-import { LoginPageComponent } from './login-page/login-page.component';
 import { GoogleSigninDirective } from './google-signin.directive';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { UserRoutingModule } from './user-routing.module';
+import { EmailLoginComponent } from './email-login/email-login.component';
 
 @NgModule({
-  declarations: [
-    LoginPageComponent,
-    GoogleSigninDirective
-  ],
-  imports: [CommonModule, SharedModule, UserRoutingModule],
+  declarations: [LoginPageComponent, GoogleSigninDirective, EmailLoginComponent],
+  imports: [CommonModule, SharedModule, UserRoutingModule, ReactiveFormsModule],
 })
 export class UserModule {}
